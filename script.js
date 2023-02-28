7aade94356b150538b7dc963c7fdfdacaa34eae7
katienotkathy <14117113+katienotkathy@users.noreply.github.com>
2023-02-27 20:31:34 -0500

    Implement grid and etch behavior. Create initial grid size button behavior

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-const container = document.querySelector(".container");
-const etchDiv = document.createElement("div");
+let gridSize = 100;
 
-etchDiv.textContent = "foo!";
-container.appendChild(etchDiv);
+// where to pick up:
+// make the button for setting the grid size
+// make a button to reset everything
+// -- consider putting all your declarations at the top for readability
+
+const container = document.querySelector(".container");
+
+function createGrid(gridSize) {
+    for (let rowNum = 0; rowNum < gridSize; rowNum++) {
+        const etchRow = document.createElement("div");
+        etchRow.id = `etchRow${rowNum}`;
+        etchRow.style.display = "flex";
+        etchRow.style.flex = "1 1 auto";
+        container.appendChild(etchRow);
+        
+        for (let colNum = 0; colNum < gridSize; colNum++) {
+            const currentRow = document.querySelector(`#etchRow${rowNum}`); 
+            const etchPixel = document.createElement("div");
+            
+            etchPixel.className = "etchPixel";
+            etchPixel.style.display = "flex";
+            etchPixel.style.flex = "1 1 auto";
+            currentRow.appendChild(etchPixel);
+        }
+    }
+}
+
+const etchPixelNodes = document.querySelectorAll(".etchPixel");
+etchPixelNodes.forEach(element => {
+    element.addEventListener('mouseover',etch);    
+});
+
+const gridButton = document.querySelector("#gridSize");
+gridButton.addEventListener('click',()=>prompt("How many pixels in the etch grid?","Choose an integer from 1-100"));
+
+function etch(e) {
+    this.style.backgroundColor = "black";
+}
+
+
+
